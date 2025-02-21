6e786cb3fafd069f58e5cf83383bec2cbde4e8d1
Henry <11198460+godzillaba@users.noreply.github.com>
2025-02-21 13:36:42 -0500

    license

--- contracts/standard/Constants.sol
+++ contracts/standard/Constants.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.13;
 
 uint256 constant BLOCK_HASH_PROVER_POINTER_SLOT = uint256(keccak256("eipXXXX.pointer.slot")) - 1;
