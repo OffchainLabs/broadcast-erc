21d1a8057a141387742ad7f81c942c25f3df18e2
Henry <11198460+godzillaba@users.noreply.github.com>
2025-02-25 13:17:27 -0500

    use svg, assign number, misc updates

--- contracts/standard/Constants.sol
+++ contracts/standard/Constants.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.13;
 
-uint256 constant BLOCK_HASH_PROVER_POINTER_SLOT = uint256(keccak256("eipXXXX.pointer.slot")) - 1;
+uint256 constant BLOCK_HASH_PROVER_POINTER_SLOT = uint256(keccak256("eip7888.pointer.slot")) - 1;
